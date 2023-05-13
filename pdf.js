9edf06d90fed0b4111845f9da8810954828552e2
Anurag <sanurag4518@gmail.com>
2023-05-14 01:23:19 +0530

    first commit

---
+++ pdf.js
@@ -0,0 +1,37 @@
+import { OpenAI } from "langchain/llms/openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { ConversationalRetrievalQAChain } from "langchain/chains";
+import * as fs from "fs";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import 'dotenv/config'
+
+const model = new OpenAI({
+  temperature: 0.9,
+  openAIApiKey: process.env.OPENAI_API_KEY,
+});
+const loader = new PDFLoader("DBMS HA.pdf");
+const text = await loader.load()
+
+ const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 1000,
+});
+const docs = await splitter.createDocuments([text + '']);
+/* Create the vectorstore */
+const vectorStore = await HNSWLib.fromDocuments(
+  docs,
+  new OpenAIEmbeddings({
+    temperature: 0.9,
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  })
+);
+console.log(docs);
+const chain = ConversationalRetrievalQAChain.fromLLM(
+  model,
+  vectorStore.asRetriever()
+);
+/* Ask it a question */
+const question = "Explain PostgreSQL?";
+const res = await chain.call({ question, chat_history: [] });
+console.log(res);
